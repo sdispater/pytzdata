fd7547f6ff0cf3b392d33c63256a5ba1bffd22c9
Seth M. Larson <SethMichaelLarson@users.noreply.github.com>
2017-03-08 09:23:43 -0600

    Add Python version classifiers.

--- setup.py
+++ setup.py
@@ -39,6 +39,8 @@ kwargs = dict(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
