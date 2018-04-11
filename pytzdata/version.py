aba5af7346bc51fb6539180e709a2696f60877c4
Sébastien Eustace <sebastien@eustace.io>
2018-04-11 09:51:53 -0500

    Bump version to 2018.4/2018d

--- pytzdata/version.py
+++ pytzdata/version.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-VERSION = '2018.3'
+VERSION = '2018.4'
 
-OLSON_VERSION = '2018c'
+OLSON_VERSION = '2018d'
--- tests/test_version.py
+++ tests/test_version.py
@@ -2,9 +2,9 @@
 
 from pytzdata.version import VERSION, OLSON_VERSION
 
-EXPECTED_VERSION = '2018.3'
+EXPECTED_VERSION = '2018.4'
 
-EXPECTED_OLSON_VERSION = '2018c'
+EXPECTED_OLSON_VERSION = '2018d'
 
 
 def test_version():
